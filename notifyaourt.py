7cc4799c5185b0fd8babc245e15c1e696a7dd44c
Kemal Maulana <kemskems12@gmail.com>
2014-04-11 19:33:31 +0900

    Add detailed info in notification messages

--- notifyaourt.py
+++ notifyaourt.py
@@ -1,34 +1,64 @@
 import os
 import os.path
 import json
-from subprocess import call, Popen, PIPE
+import subprocess
 
-def get_version(pkgs):
-    """Get the current version from a list of packages"""
+
+VERSION_KEY = 'Version'
+URL_KEY = 'URL'
+
+
+def get_info(pkgs):
+    """Get the current info of a list of packages"""
     pkg_map = {}
     for pkg in pkgs:
-        yaourt_proc = Popen(['yaourt', '-Qi', pkg], stdout=PIPE)
-        grep_proc = Popen(['grep', 'Version'], stdin=yaourt_proc.stdout,
-            stdout=PIPE)
+        yaourt_proc = subprocess.Popen(['yaourt', '-Qi', pkg],
+            stdout=subprocess.PIPE)
+        pattern = VERSION_KEY + '|' + URL_KEY
+        grep_proc = subprocess.Popen(['grep', '-E', pattern],
+            stdin=yaourt_proc.stdout, stdout=subprocess.PIPE)
         out, err = grep_proc.communicate()
-        ver = str(out, encoding='utf-8').split()[2]
-        pkg_map[pkg] = ver
+        sout = str(out, encoding='utf-8')
+        info_map = {u.split()[0]: u.split()[2] for u in sout.split('\n')
+            if len(u) > 0}
+        pkg_map[pkg] = info_map
     return pkg_map
 
+
 def get_packages():
-    """Get the user-defined important list of packages"""
+    """Get the user-defined important list of packages
+
+    List of packages are stored as JSON file in $HOME/.notifyaourt.conf
+
+    """
     home_dir = os.environ['HOME']
     config_file = os.path.join(home_dir, '.notifyaourt.conf')
-    lines = ""
-    with open(config_file) as conf:
-        for line in conf:
-            lines += line
-    return json.loads(lines)
+    if os.path.exists(config_file):
+        lines = ""
+        with open(config_file) as conf:
+            for line in conf:
+                lines += line
+        return json.loads(lines)
+    else:
+        return []
+
 
 packages = get_packages()
-curr_version = get_version(packages)
-call(['yaourt', '-Syua'])
-new_version = get_version(packages)
+old_info = get_info(packages)
+subprocess.call(['yaourt', '-Syua'])
+new_info = get_info(packages)
+updated_packages = []
 for pkg in packages:
-    if curr_version[pkg] != new_version[pkg]:
-        print(pkg, 'has been updated!')
+    if old_info[pkg][VERSION_KEY] != new_info[pkg][VERSION_KEY]:
+        updated_packages.append(pkg)
+
+print()
+if len(updated_packages) == 0:
+    print('No important package updates')
+else:
+    print('Important package updates:')
+    for cnt, pkg in enumerate(updated_packages, start=1):
+        old_ver = old_info[pkg][VERSION_KEY]
+        new_ver = new_info[pkg][VERSION_KEY]
+        url = new_info[pkg][URL_KEY]
+        print(cnt, ')', pkg, old_ver, '=>', new_ver, '|', url)
