0ef5a338f064f53571eda72bbfdb8db42f4c11f7
Kemal Maulana <kemskems12@gmail.com>
2014-04-11 17:15:11 +0900

    Put list of packages in a config file

--- notifyaourt.py
+++ notifyaourt.py
@@ -1,7 +1,10 @@
+import os
+import os.path
+import json
 from subprocess import call, Popen, PIPE
 
 def get_version(pkgs):
-    """Get the current version of packages"""
+    """Get the current version from a list of packages"""
     pkg_map = {}
     for pkg in pkgs:
         yaourt_proc = Popen(['yaourt', '-Qi', pkg], stdout=PIPE)
@@ -12,7 +15,17 @@ def get_version(pkgs):
         pkg_map[pkg] = ver
     return pkg_map
 
-packages = ['git']
+def get_packages():
+    """Get the user-defined important list of packages"""
+    home_dir = os.environ['HOME']
+    config_file = os.path.join(home_dir, '.notifyaourt.conf')
+    lines = ""
+    with open(config_file) as conf:
+        for line in conf:
+            lines += line
+    return json.loads(lines)
+
+packages = get_packages()
 curr_version = get_version(packages)
 call(['yaourt', '-Syua'])
 new_version = get_version(packages)
